ac95363ca095b8b17b53aa60dc1d41e9478e39d2
dcsp3 <daksheshpandey@gmail.com>
2023-08-01 11:51:41 +0100

    add total pnl and roi to view

--- tracker/views.py
+++ tracker/views.py
@@ -8,6 +8,8 @@ def index(request):
     tracker = CoinbaseTracker()
     return render(request, "tracker/index.html", {
         "value": tracker.getTotalPortfolioValue(),
+        "totalpnl": tracker.getTotalPnL(),
+        "totalroi": tracker.getTotalROI(),
         "currency_value_pairs": tracker.getCurrencyValuePairs(),
         "currencies": [i.currency for i in tracker.currencies][::-1],
         "allocations": [tracker.getCurrencyAllocationPercent(i) for i in tracker.currencies][::-1],
